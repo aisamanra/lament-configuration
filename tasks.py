8d81b43996408da29673c005557e7b812bf9787c
Getty Ritter <gettylefou@gmail.com>
2020-04-05 16:53:49 -0700

    Add install task to tasks, for good measure

--- tasks.py
+++ tasks.py
@@ -10,3 +10,7 @@ def run(c, port=8080, host='127.0.0.1'):
         f'poetry run python -m flask run -p {port} -h {host}',
         env={'FLASK_APP': 'lc/main.py'},
     )
+
+@task
+def install(c):
+    c.run('poetry install')
