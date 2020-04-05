87ddc761d26e96873debbfcf04362b7efea62051
Getty Ritter <gettylefou@gmail.com>
2020-04-05 16:16:09 -0700

    Defer db initialization for testing

--- lc/config.py
+++ lc/config.py
@@ -2,5 +2,4 @@ import os
 
 import playhouse.sqlite_ext
 
-DB_LOC=os.getenv('SCRAPBOARD', 'test.db')
-DB=playhouse.sqlite_ext.SqliteExtDatabase(DB_LOC)
+DB=playhouse.sqlite_ext.SqliteExtDatabase(None)
