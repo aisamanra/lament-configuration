999eb44b14b2cb004124d6452d44d7abddd2adc6
Getty Ritter <gettylefou@gmail.com>
2020-04-05 16:54:38 -0700

    Add docstrings to tests

--- tasks.py
+++ tasks.py
@@ -2,10 +2,12 @@ from invoke import task
 
 @task
 def test(c):
+    '''Run all the provided tests'''
     c.run('poetry run python -m pytest tests/*.py')
 
 @task
 def run(c, port=8080, host='127.0.0.1'):
+    '''Run a debug server locally'''
     c.run(
         f'poetry run python -m flask run -p {port} -h {host}',
         env={'FLASK_APP': 'lc/main.py'},
@@ -13,4 +15,5 @@ def run(c, port=8080, host='127.0.0.1'):
 
 @task
 def install(c):
+    '''Install the listed dependencies into a virtualenv'''
     c.run('poetry install')
