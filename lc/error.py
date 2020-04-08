dc4f7d917de53081ef238f9e7b8cd75b83bc0f06
Getty Ritter <gettylefou@gmail.com>
2020-04-07 20:12:41 -0700

    Refactor exception family for better utility elsewhere

--- lc/error.py
+++ lc/error.py
@@ -1,9 +1,26 @@
 from dataclasses import dataclass
 
+class LCException(Exception):
+    def to_json(self) -> dict:
+        return {"error": str(self)}
 
 @dataclass
-class UserExists(Exception):
+class UserExists(LCException):
     name: str
 
     def __str__(self):
         return f"A user named {self.name} already exists."
+
+@dataclass
+class NoSuchUser(LCException):
+    name: str
+
+    def __str__(self):
+        return f"No user named {self.name} exists."
+
+@dataclass
+class BadPassword(LCException):
+    name: str
+
+    def __str__(self):
+        return f"Wrong password for user {self.name}."
