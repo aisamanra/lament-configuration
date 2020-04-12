636e6e2819d9886298f9d865cda69b9e15b3d2a6
Getty Ritter <samothes@infinitenegativeutility.com>
2020-04-12 16:25:11 -0700

    Add config to the populate script

--- scripts/populate.py
+++ scripts/populate.py
@@ -2,6 +2,7 @@
 
 import datetime
 import json
+import os
 
 import lc.config as c
 import lc.model as m
@@ -14,7 +15,7 @@ def main():
 
     u = m.User.get_or_none(name="gdritter")
     if not u:
-        u = m.User.from_request(r.User(name="gdritter", password="behest",))
+        u = m.User.from_request(r.User(name="gdritter", password=os.getenv("PASSWORD", "behest").strip(),))
         u.set_as_admin()
 
     c.log(f"created user {u.name}")
